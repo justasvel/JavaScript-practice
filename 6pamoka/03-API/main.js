428c1f559c69aa6c82d627f8dafcba211ba2e1f1
Justas <justasvee@gmail.com>
2021-01-25 19:58:39 +0200

    Added API

--- 6pamoka/03-API/main.js
+++ 6pamoka/03-API/main.js
@@ -1,6 +1,17 @@
-fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then(data => console.log(data));
+const getRandomUser = async () => {
+    const response = await fetch('https://randomuser.me/api/');
 
-    for
+    if (response.status === 200) {
+        const data = await response.json();
 
+        return data;
+    } else {
+        throw new Error('Unable to get the user');
+    }
+}
+
+getRandomUser().then(randUser => {
+    let user = randUser.results[0];
+
+    console.log(user)
+});
