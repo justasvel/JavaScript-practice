553d7e1686936785de92241c78c92d66437d95ef
Justas <justasvee@gmail.com>
2021-01-28 14:55:53 +0200

    6 pamokos 3 uzduoties sprendimas

--- 6pamoka/03-API/main.js
+++ 6pamoka/03-API/main.js
@@ -13,5 +13,18 @@ const getRandomUser = async () => {
 getRandomUser().then(randUser => {
     let user = randUser.results[0];
 
+    let cellphone = document.querySelector('#cell');
+    let name = document.querySelector('#name');
+    let image = document.querySelector('#image');
+    let gender = document.querySelector('#gender');
+    let date = document.querySelector('#date');
+
+
+
+    cellphone.innerHTML = user.cell;
+    name.innerHTML = user.name.first + ' ' + user.name.last;
+    image.src = user.picture.medium;
+    gender.innerHTML = user.gender;
+    date.innerHTML = user.dob.date;
     console.log(user)
 });
